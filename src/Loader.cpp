dfe4d68cc329d3034a13b32c2a74c672b2680a86
Armand <jurkowski.armand@gmail.com>
2026-07-17 00:21:51 +0200

    Phase 1: Bar struct + Loader skeleton, core library in CMake

---
+++ src/Loader.cpp
@@ -0,0 +1,8 @@
+#include "../src/Loader.hpp"
+
+std::vector<Bar> loadBarsFromCSV(const std::string& path) {
+    std::vector<Bar> bars;
+    // TODO read the CSV file
+    return bars;
+}
+
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,12 @@
 #include <catch2/catch_test_macros.hpp>
+#include "../src/Bar.hpp"
 
 TEST_CASE("skeleton sanity check") {
     REQUIRE(1 + 1 == 2);
+}
+
+TEST_CASE("Bar stores its field") {
+    Bar b; // bar b is 
+    b.close = 100.5;
+    REQUIRE(b.close == 100.5);
 }
