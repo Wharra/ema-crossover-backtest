2db7dda7aee6c195070dc20d78229bedd53372c1
Armand <jurkowski.armand@gmail.com>
2026-07-29 20:24:55 +0200

    Add CSV loader with tests

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ ctest --test-dir build --output-on-failure    # 3 lance les tests
 
 #include <iostream>
 #include "../src/Bar.hpp"
+#include "../src/Loader.hpp"
 
 int main() {
     std::cout << "EMA crossover backtester - Salut c'est Armand!\n";
